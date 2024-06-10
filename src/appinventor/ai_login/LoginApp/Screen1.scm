b7c5c62c10c593ec1c5751184a4b771c84484344
BCruzLeon <bcruzleon7@gmail.com>
2024-06-09 23:50:20 -0500

    Add files via upload

---
+++ src/appinventor/ai_login/LoginApp/Screen1.scm
@@ -0,0 +1,88 @@
+
+#|$JSON
+{
+  "authURL": "",
+  "YaVersion": "182",
+  "Source": "Form",
+  "YaBuildVersion": "69",
+  "Name": "Screen1",
+  "Type": "Form",
+  "Version": "27",
+  "Properties": {
+    "$Components": [
+      {
+        "Name": "VerticalArrangement1",
+        "Type": "VerticalArrangement",
+        "Properties": {
+          "Width": "(-2)",
+          "Height": "(-2)",
+          "$Components": [
+            {
+              "Name": "Image1",
+              "Type": "Image",
+              "Properties": {
+                "Width": "(0)",
+                "Height": "(0)"
+              }
+            },
+            {
+              "Name": "Label1",
+              "Type": "Label",
+              "Properties": {
+                "Text": "Iniciar sesión",
+                "FontSize": "24"
+              }
+            },
+            {
+              "Name": "Label2",
+              "Type": "Label",
+              "Properties": {
+                "Text": "Inicia sesión para continuar",
+                "FontSize": "14"
+              }
+            },
+            {
+              "Name": "TextBox1",
+              "Type": "TextBox",
+              "Properties": {
+                "Hint": "Correo",
+                "Width": "(-2)"
+              }
+            },
+            {
+              "Name": "PasswordTextBox1",
+              "Type": "PasswordTextBox",
+              "Properties": {
+                "Hint": "Password",
+                "Width": "(-2)"
+              }
+            },
+            {
+              "Name": "Button1",
+              "Type": "Button",
+              "Properties": {
+                "Text": "Iniciar sesión",
+                "Width": "(-2)",
+                "BackgroundColor": "&HFF000000",
+                "TextColor": "&HFFFFFFFF"
+              }
+            },
+            {
+              "Name": "Label3",
+              "Type": "Label",
+              "Properties": {
+                "Text": "Registrarse",
+                "TextColor": "&HFF0000FF"
+              }
+            }
+          ]
+        }
+      }
+    ],
+    "AppName": "LoginApp",
+    "Title": "Screen1",
+    "AlignHorizontal": "3",
+    "AlignVertical": "2"
+  }
+}
+|#
